bdcbed495efd0bfaf17e98f1568ef14cacc21004
Huy TA QUOC <htquoc@linagora.com>
2020-08-13 13:28:28 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-inbox/issues/53: Added missing dependency 'linshare-api-client'

--- src/app/services/linshare-api-client.wrapper.js
+++ src/app/services/linshare-api-client.wrapper.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const { Client } = require('linshare-api-client');
+
 angular.module('linagora.esn.linshare')
-  .factory('LinshareApiClient', function($window) {
-    return $window.LinshareApiClient;
+  .factory('LinshareApiClient', function() {
+    return Client;
   });
